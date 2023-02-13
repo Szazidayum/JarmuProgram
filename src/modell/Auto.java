a326cf69c488ae66d85fbafd8499ff79cf17c879
Szazidayum <szabrinamolnar@gmail.com>
2023-02-13 10:00:02 +0100

    JarmuProgram Auto osztaly konstruktor javitasa

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -1,9 +1,14 @@
 
-package main;
+package modell;
 
-public class Auto {
+public class Auto extends Jarmu{
     
     private boolean defekt;
+
+    public Auto(boolean defekt, boolean beinditva, boolean uzemanyag, boolean megerkezett) {
+        super(beinditva, uzemanyag, megerkezett);
+        this.defekt = defekt;
+    } 
     
     public void kereketCserel(){
         
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -1,8 +1,14 @@
-package main;
+package modell;
 
 public abstract class Jarmu {
 
     private boolean beinditva, uzemanyag, megerkezett;
+
+    public Jarmu(boolean beinditva, boolean uzemanyag, boolean megerkezett) {
+        this.beinditva = beinditva;
+        this.uzemanyag = uzemanyag;
+        this.megerkezett = megerkezett;
+    }
     
     public void beindit(){
         
